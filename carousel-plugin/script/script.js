4ef74dd8961e754ff67b431e07b60ee5d95f6928
taisa-queirozz <48928146+taisa-queirozz@users.noreply.github.com>
2019-08-29 11:31:25 -0300

    teste

---
+++ carousel-plugin/script/script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
